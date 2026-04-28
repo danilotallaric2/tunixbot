cc4e77e984b67a379d6666d9fcb65e50c7799568
Danilo <danilotallarico@icloud.com>
2026-04-28 20:27:41 +0200

    test

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,5 +1,7 @@
 const { errorEmbed } = require('../utils/embeds');
 const { safeReply } = require('../utils/reply');
+const logger = require('../utils/logger');
+const { inspect } = require('util');
 
 module.exports = {
   name: 'interactionCreate',
@@ -18,6 +20,19 @@ module.exports = {
     try {
       await command.execute(interaction);
     } catch (error) {
+      const context = {
+        command: interaction.commandName,
+        userId: interaction.user?.id,
+        guildId: interaction.guildId,
+        channelId: interaction.channelId
+      };
+
+      logger.error(
+        `Command failed: ${context.command} (user=${context.userId} guild=${context.guildId} channel=${context.channelId})`,
+        error
+      );
+      logger.error('Command error details', inspect(error, { depth: 6, colors: false }));
+
       await safeReply(interaction, {
         embeds: [errorEmbed('Errore Comando', error.message || 'Si e verificato un errore durante il comando.')],
         ephemeral: true
